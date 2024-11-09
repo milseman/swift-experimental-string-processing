3647b89b775ca8d0f3bde356ac137128dc7979a1
Michael Ilseman <michael.ilseman@gmail.com>
2024-11-09 09:50:01 -0700

    Benchmark: Add not-found and all-found path variants

--- Sources/RegexBenchmark/Inputs/FSPaths.swift
+++ Sources/RegexBenchmark/Inputs/FSPaths.swift
@@ -1,5 +1,5 @@
 // Successful match FSPaths
-private let fsPathSuccess = #"""
+private let pathSuccess = #"""
 ./First/Second/Third/some/really/long/content.extension/more/stuff/OptionLeft
 ./First/Second/Third/some/really/long/content.extension/more/stuff/OptionRight
 ./First/Second/PrefixThird/some/really/long/content.extension/more/stuff/OptionLeft
@@ -10,7 +10,7 @@ private let fsPathSuccess = #"""
 //
 // We will have far more failures than successful matches by interspersing
 // this whole list between each success
-private let fsPathFailure = #"""
+private let pathFailure = #"""
 a/b/c
 /smol/path
 /a/really/long/path/that/is/certainly/stored/out/of/line
@@ -28,15 +28,22 @@ a/b/c
 ./First/Second/PrefixThird/some/really/long/content/more/stuff/OptionRight
 """#
 
+private func listify(_ s: String) -> [String] {
+  s.split(whereSeparator: { $0.isNewline }).map { String($0) }
+}
+
+private let pathSuccessList: [String] = { listify(pathSuccess) }()
+private let pathFailureList: [String] = { listify(pathFailure) }()
+
 extension Inputs {
   static let fsPathsList: [String] = {
-    var result: [String] = []
-    let failures: [String] = fsPathFailure.split(whereSeparator: { $0.isNewline }).map { String($0) }
-    result.append(contentsOf: failures)
+    var result = pathFailureList
+    result.append(contentsOf: pathFailureList)
 
-    for success in fsPathSuccess.split(whereSeparator: { $0.isNewline }) {
+    for success in pathSuccessList {
       result.append(String(success))
-      result.append(contentsOf: failures)
+      result.append(contentsOf: pathFailureList)
+      result.append(contentsOf: pathFailureList)
     }
 
     // Scale result up a bit
@@ -47,5 +54,16 @@ extension Inputs {
 
     return result
 
+  }()
+
+  static let fsPathsNotFoundList: [String] = {
+    var result = pathFailureList
+    result.append(contentsOf: pathFailureList)
+    result.append(contentsOf: result)
+    result.append(contentsOf: result)
+    result.append(contentsOf: result)
+    result.append(contentsOf: result)
+    result.append(contentsOf: result)
+    return result
   }()
 }
